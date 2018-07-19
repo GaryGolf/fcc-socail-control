c131db17fabc9427352e862346f34a59a75c1c44
GaryGolf <garygolf@mail.ru>
2018-07-19 16:50:48 +0300

    create user page component

---
+++ src/components/user-page.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as styles from './user-page.css';
+
+interface Props {
+  html: string;
+}
+
+const UserPage:React.SFC<Props>  = ({ html }) => {
+
+  if (!html) return null;
+  
+  const handleClick = (event:React.MouseEvent<HTMLDivElement>) => event.stopPropagation();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.shield} onClick={handleClick}/>
+      <div  dangerouslySetInnerHTML={{__html:html}}/>
+    </div>
+  )
+}
+
+export default UserPage;
