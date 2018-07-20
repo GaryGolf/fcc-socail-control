5b2564086eff7badb7ab47dd5c92b0b958648950
GaryGolf <garygolf@mail.ru>
2018-07-20 13:51:59 +0300

    add airbnb extractors to config

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,23 @@ export const config = {
         const title = xml.querySelector('title').textContent;
         const end = title.indexOf('\'');
         return title.substring(0, end);
+      },
+      country: xml => xml.querySelector('#site-content')
+        .querySelector('a.link-reset').textContent,
+      joined: xml => xml.querySelector('#site-content')
+        .querySelector('span.text-normal').textContent.trim().slice(10),
+      verified: xml => {
+        const nodelist = xml.querySelector('#user').parentElement
+          .querySelector('ul[title]').childNodes;
+        
+        const labels = []
+
+        for(var i = 0; i < nodelist.length; i++) {
+          if (nodelist[i].nodeName == 'LI') 
+            labels.push(nodelist[i].childNodes[1].textContent.trim().toLowerCase())
+        }
+        
+        return labels.join(', ')
       }
       
     }
